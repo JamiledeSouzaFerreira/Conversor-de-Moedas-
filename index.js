23c0e87c16d15e5ee39667076a4234e51e6666bf
Jamile de Souza Ferreira <jj2003.011@gmail.com>
2022-02-24 17:38:07 -0300

    html básico

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
- var real = Number.parseFloat(window.prompt('Digite o valor em real'))
- var conversão = real/5
- window.alert('O valor corresponde à ' + conversão + ' dólares' )
+ //var real = Number.parseFloat(window.prompt('Digite o valor em real'))
+ //var conversão = real/5
+ //window.alert('O valor corresponde à ' + conversão + ' dólares' )
  
