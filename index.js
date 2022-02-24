9af7494d7e30e85b9e4402c4b78221209c0a6351
Jamile de Souza Ferreira <jj2003.011@gmail.com>
2022-02-24 18:00:04 -0300

    js básico

--- index.js
+++ index.js
@@ -3,3 +3,24 @@
  //var conversão = real/5
  //window.alert('O valor corresponde à ' + conversão + ' dólares' )
  
+ function converter (){
+     var txt = document.querySelector('input#txt')
+     var txt = Number(txt.value)
+     var res = document.querySelector('p#resposta')
+     var rad = document.getElementsByName('radmoney')
+     var moeda = ''
+     //pra usarmos os radios precisaremos de uma varíavel que os represente pelo nome, e outra vazia que os represente individualmente
+     if (rad[0].checked ){
+         var dólar = txt / 5
+        res.innerHTML = (` $${dólar} doláres`)
+     } else if (rad[1].checked){
+        var libra = txt / 7
+        res.innerHTML = (`$${libra} libras`)
+     } else if(rad[2].checked){
+         var euro = txt / 5.7
+         res.innerHTML = (`$${euro} euros`)
+     } else {
+         window.alert(' Por favor selecione uma moeda')
+     }
+
+ }
